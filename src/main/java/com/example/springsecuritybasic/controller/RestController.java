1dac2ddd6597a0eed6465f9fe47e63e6d7cd8b10
hamzaacode <amir.hamza.code@gmail.com>
2023-10-10 17:29:38 +0600

    Added customized default spring security config.

--- src/main/java/com/example/springsecuritybasic/controller/RestController.java
+++ src/main/java/com/example/springsecuritybasic/controller/RestController.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-@GetMapping("welcome") 
-    public String welcome(){
+    @GetMapping("/welcome")
+    public String welcome() {
         return "Welcome to spring security test project";
+    }
+
+    @GetMapping("/userInfo")
+    public String getUserInfo() {
+        return "get user info";
+    }
+
+    @GetMapping("/getContactInfo")
+    public String getContactInfo() {
+        return "get contact info";
     }
 }
