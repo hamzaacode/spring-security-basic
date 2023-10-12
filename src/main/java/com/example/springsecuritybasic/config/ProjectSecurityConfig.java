fe9a3a894dfb9728e9f8a638c6d99e2a798901de
hamzaacode <amir.hamza.code@gmail.com>
2023-10-12 10:11:31 +0600

    Configure to deny all the request.

--- src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -5,15 +5,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/userInfo").authenticated();
-        http.authorizeHttpRequests().requestMatchers("/welcome", "/getContactInfo").permitAll();
-        http.formLogin();
-        http.httpBasic();
-        return (SecurityFilterChain) http.build();
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(request -> request.anyRequest().denyAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
     }
+
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+//        http.authorizeHttpRequests(request -> request.anyRequest().permitAll());
+//        http.formLogin(withDefaults());
+//        http.httpBasic(withDefaults());
+//        return http.build();
+//    }
+
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        //  http.authorizeHttpRequests(request -> request.anyRequest().permitAll());
+//        http.authorizeHttpRequests().requestMatchers("/userInfo").authenticated();
+//        http.authorizeHttpRequests().requestMatchers("/welcome", "/getContactInfo").permitAll();
+//        http.formLogin();
+//        http.httpBasic();
+//        return (SecurityFilterChain) http.build();
+//    }
 }
