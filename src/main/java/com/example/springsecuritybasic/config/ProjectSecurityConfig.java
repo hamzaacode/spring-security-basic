2d9e8d23f179a9a3545e552712d9b09725baac21
hamzaacode <amir.hamza.code@gmail.com>
2023-10-12 15:04:21 +0600

    Configuring Users using InMemoryUserDetailManager Approach - 2

--- src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -38,21 +38,40 @@ public class ProjectSecurityConfig {
         return (SecurityFilterChain) http.build();
     }
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("password")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user1 = User.withDefaultPasswordEncoder()
+//                .username("amir")
+//                .password("password")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user1);
+//    }
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password("password")
                 .authorities("admin")
                 .build();
 
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("amir")
+        UserDetails user1 = User.withUsername("user1")
                 .password("password")
                 .authorities("read")
                 .build();
-
         return new InMemoryUserDetailsManager(admin, user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
