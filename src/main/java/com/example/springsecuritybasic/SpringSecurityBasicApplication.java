e38e9bc0c9c2032af02f79506b0ea580d8f9c325
hamzaacode <amir.hamza.code@gmail.com>
2023-10-16 17:07:55 +0600

    - deleted commented code

--- src/main/java/com/example/springsecuritybasic/SpringSecurityBasicApplication.java
+++ src/main/java/com/example/springsecuritybasic/SpringSecurityBasicApplication.java
@@ -3,8 +3,6 @@ package com.example.springsecuritybasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableJpaRepositories("com.example.otherPackage") //if repository and model defines outside of base package (com.example.springsecuritybasic)
-//@EntityScan("com.example.otherPackage")
 @SpringBootApplication
 public class SpringSecurityBasicApplication {
 
