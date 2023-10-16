4859ca80b4c0fa512b7b1df2f3a82d606de89e9b
hamzaacode <amir.hamza.code@gmail.com>
2023-10-16 15:03:07 +0600

    [Authentication using JDBC user details managers.]
    - Created custom database (Easybankn) & custom table users,authorities,customer
    - created entity model customer and its repository
    - Added dependency of Spring data JPA, JDBC and Mysql Connector.

--- src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -3,31 +3,16 @@ package com.example.springsecuritybasic.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
-
-//    @Bean
-//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-//        http.authorizeHttpRequests(request -> request.anyRequest().denyAll());
-//        http.formLogin(withDefaults());
-//        http.httpBasic(withDefaults());
-//        return http.build();
-//    }
-
-//    @Bean
-//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-//        http.authorizeHttpRequests(request -> request.anyRequest().permitAll());
-//        http.formLogin(withDefaults());
-//        http.httpBasic(withDefaults());
-//        return http.build();
-//    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -38,38 +23,10 @@ public class ProjectSecurityConfig {
         return (SecurityFilterChain) http.build();
     }
 
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("password")
-//                .authorities("admin")
-//                .build();
-//
-//        UserDetails user1 = User.withDefaultPasswordEncoder()
-//                .username("amir")
-//                .password("password")
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user1);
-//    }
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-                .password("password")
-                .authorities("admin")
-                .build();
-
-        UserDetails user1 = User.withUsername("user1")
-                .password("password")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user1);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
