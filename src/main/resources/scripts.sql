96406bd8babe4edc6db52cb698392c595209300a
hamzaacode <amir.hamza.code@gmail.com>
2023-10-16 16:21:03 +0600

    [Authentication using Custom user details managers.]
    - Added custom user details service implementation
    - Removed JDBC user details implementation
    - Added Custom table customer , entity.

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -15,5 +15,17 @@ CREATE TABLE `authorities` (
   `authority` varchar(45) NOT NULL,
   PRIMARY KEY (`id`));
 
+
 INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '12345', '1');
 INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+ VALUES ('johndoe@example.com', '54321', 'admin');
