88644bd4532cd9baa895241a8ad175c0b6baa181
hamzaacode <amir.hamza.code@gmail.com>
2023-10-18 17:07:17 +0600

    - Added CORS support

--- src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -1,21 +1,37 @@
 package com.example.springsecuritybasic.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config=new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and().csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/userInfo").authenticated()
-                .requestMatchers("/welcome", "/getContactInfo", "/register").permitAll()
+                .requestMatchers("/user","/myAccount").authenticated()
+                .requestMatchers("/welcome","/register","/contact","/notices").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
