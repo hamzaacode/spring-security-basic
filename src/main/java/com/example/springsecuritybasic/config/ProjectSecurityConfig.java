9b6586c1b7b885ecf7b02f3304cf72de135a422c
hamzaacode <amir.hamza.code@gmail.com>
2023-10-19 10:48:48 +0600

    - Added CSRF token configuratioin, also added CSRF ignore case - Added CsrfCookieFilter chain and configure to do filter after BasicAuthenticationFilter -

--- src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.springsecuritybasic.config;
 
+import com.example.springsecuritybasic.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -17,10 +22,19 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(new CorsConfigurationSource() {
+
+        // Creating CSRF token
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
+        // Creating Jsession Id
+        http.securityContext().requireExplicitSave(false)// to create Jsession id after initial login
+                .and().sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))// and send  to this Jsession into UI Application
+                // configuring cors
+                .cors().configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config=new CorsConfiguration();
+                        CorsConfiguration config = new CorsConfiguration();
                         config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowedHeaders(Collections.singletonList("*"));
@@ -28,10 +42,14 @@ public class ProjectSecurityConfig {
                         config.setMaxAge(3600L);
                         return config;
                     }
-                }).and().csrf().ignoringRequestMatchers("/register","/contact").and()
+                }).and()
+                // configuring CSRF token with ignore case
+                .csrf((csrf) -> csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler).ignoringRequestMatchers("/register", "/contact")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class) // telling spring security, please do filter CsrfCookieFilter after BasicAuthenticationFilter
                 .authorizeHttpRequests()
-                .requestMatchers("/user","/myAccount").authenticated()
-                .requestMatchers("/welcome","/register","/contact","/notices").permitAll()
+                .requestMatchers("/user", "/myAccount").authenticated()
+                .requestMatchers("/welcome", "/register", "/contact", "/notices").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
